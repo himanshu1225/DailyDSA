a1e69a8bf8f104cf1791478a7b8fba6b03fa7848
Himanshu bhati <61904931+himanshu1225@users.noreply.github.com>
2024-01-24 22:07:49 +0530

    Time: 1 ms (98.30%), Space: 42.9 MB (31.28%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -10,11 +10,13 @@ class Solution {
     }
     
     private boolean containEvenDigits(int num){
-        int count = 0;
-        while(num > 0){
-            count++;
-            num = num/10;
-        }
+        // int count = 0;
+        // while(num > 0){
+        //     count++;
+        //     num = num/10;
+        // }
+        // return count % 2 == 0;
+        int count = (int) Math.log10(num) + 1;
         return count % 2 == 0;
     }
 }
