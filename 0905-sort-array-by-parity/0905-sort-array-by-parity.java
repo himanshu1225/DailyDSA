5d09edcf14bb0192d3d2c21fe19acf5db96f8554
Himanshu bhati <61904931+himanshu1225@users.noreply.github.com>
2024-01-26 22:41:05 +0530

    Time: 1 ms (72.21%), Space: 44.6 MB (64.15%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -8,6 +8,7 @@ class Solution {
             if(nums[end] %2 != 0){
                 end--;
             }
+//             start < end for 2 size case; (4, 1)  ---> s++ , e-- now e->4 and s->1 if s < e was not present then fir se swap ho jata
             if(start < end && nums[start] % 2 != 0 && nums[end] % 2 == 0){
                 int temp = nums[start];
                 nums[start] = nums[end];
