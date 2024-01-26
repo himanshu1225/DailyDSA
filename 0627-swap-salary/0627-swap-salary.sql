c536813f8585f3fd559526b1e547977381f82ec6
Himanshu bhati <61904931+himanshu1225@users.noreply.github.com>
2024-01-26 20:23:42 +0530

    Time: 484 ms (46.74%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,2 +1,10 @@
 # Write your MySQL query statement below
 UPDATE Salary SET sex = IF(sex = 'm', 'f','m');
+# here it is like ternary operator if sex = 'm' then f otherwise m
+# another way is to use CASE WHEN 
+# can have multiple WHEN
+# UPDATE salary SET sex =
+# CASE sex
+#     WHEN 'm' THEN 'f'
+#     ELSE 'm'
+# END;
