1b2f50d1a0a2eb80493dbae892be1522fa41d185
Himanshu bhati <61904931+himanshu1225@users.noreply.github.com>
2024-01-26 22:54:36 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (64.15%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -2,20 +2,19 @@ class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int start = 0, end = nums.length-1;
         while(start < end){
-            if(nums[start] % 2 == 0){
-                start++;
-            }
-            if(nums[end] %2 != 0){
+           while(start < end && nums[start] % 2 == 0){
+               start++;
+           }
+           while(start < end && nums[end] % 2 != 0){
                 end--;
-            }
-//             start < end for 2 size case; (4, 1)  ---> s++ , e-- now e->4 and s->1 if s < e was not present then fir se swap ho jata
-            if(start < end && nums[start] % 2 != 0 && nums[end] % 2 == 0){
-                int temp = nums[start];
-                nums[start] = nums[end];
-                nums[end] = temp;
-                start++;
-                end--;
-            }
+           }
+           if(start < end){
+               int temp = nums[start];
+               nums[start] = nums[end];
+               nums[end] = temp;
+               start++;
+               end--;
+           } 
         }
         return nums;
     }
