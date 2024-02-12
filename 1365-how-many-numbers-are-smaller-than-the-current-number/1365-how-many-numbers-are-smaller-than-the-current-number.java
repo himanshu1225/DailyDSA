987eadeac88bd1128c4a4a5ea8d4fefb94a46a35
Himanshu bhati <61904931+himanshu1225@users.noreply.github.com>
2024-02-12 11:55:19 +0530

    Time: 1 ms (100.00%), Space: 44.6 MB (14.28%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,15 +1,29 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int n = nums.length;
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++){
-            int cnt = 0;
-            for(int j = 0; j < n; j++){
-                if(j != i && nums[j] < nums[i]) {
-                    cnt++;
-                }
+//  brute force: o(n2)
+// Count sort technique: As in constraits number of elements are from 0 to 100 samll range.
+// optimize the solution by using a counting sort technique to count the occurrences of each number and then use this information to determine the count of smaller numbers than the current number.
+//  Time: o(n) and space is O(101) + o(n)       
+        int[] freq = new int[101];
+        for(int i = 0; i < nums.length; i++){
+            freq[nums[i]]++;
+        }
+// count of each element is computed. [0,1,2,1,0,0,0,0,1]
+// will calculate running sum
+        for(int i = 1; i < 101; i++){
+            freq[i] = freq[i] + freq[i-1];
+        }
+//  running sum: [0,1,3,4,4,4,4,4,5]
+//                0 1 2 3 4 5 6 7 8
+// This signifies that there are 4 elemets(i-1) which is less than 8, 1 element less than 2 (i-1)
+        int [] ans= new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+//          [5,0,10,0,10,6]  -- > if condition is if element is zero.
+            if(nums[i] == 0){
+                ans[i] = 0;
+            }else{
+                ans[i] = freq[nums[i] -1];   
             }
-            ans[i] = cnt;
         }
         return ans;
     }
